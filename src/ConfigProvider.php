4fd758469676b9b3cd665f4a4916427e84b11ef0
Matthew Weier O'Phinney <matthew@weierophinney.net>
2019-12-31 09:45:32 -0600

    Rewriting as Laminas Project package

--- src/ConfigProvider.php
+++ src/ConfigProvider.php
@@ -1,18 +1,19 @@
 <?php
+
 /**
- * @see       https://github.com/zendframework/zend-problem-details for the canonical source repository
- * @copyright Copyright (c) 2017 Zend Technologies USA Inc. (https://www.zend.com)
- * @license   https://github.com/zendframework/zend-problem-details/blob/master/LICENSE.md New BSD License
+ * @see       https://github.com/mezzio/mezzio-problem-details for the canonical source repository
+ * @copyright https://github.com/mezzio/mezzio-problem-details/blob/master/COPYRIGHT.md
+ * @license   https://github.com/mezzio/mezzio-problem-details/blob/master/LICENSE.md New BSD License
  */
 
 declare(strict_types=1);
 
-namespace Zend\ProblemDetails;
+namespace Mezzio\ProblemDetails;
 
 /**
  * Configuration provider for the package.
  *
- * @see https://docs.zendframework.com/zend-component-installer/
+ * @see https://docs.laminas.dev/laminas-component-installer/
  */
 class ConfigProvider
 {
@@ -32,6 +33,12 @@ class ConfigProvider
     public function getDependencies() : array
     {
         return [
+            // Legacy Zend Framework aliases
+            'aliases' => [
+                \Zend\ProblemDetails\ProblemDetailsMiddleware::class => ProblemDetailsMiddleware::class,
+                \Zend\ProblemDetails\ProblemDetailsNotFoundHandler::class => ProblemDetailsNotFoundHandler::class,
+                \Zend\ProblemDetails\ProblemDetailsResponseFactory::class => ProblemDetailsResponseFactory::class,
+            ],
             'factories'  => [
                 ProblemDetailsMiddleware::class => ProblemDetailsMiddlewareFactory::class,
                 ProblemDetailsNotFoundHandler::class => ProblemDetailsNotFoundHandlerFactory::class,
