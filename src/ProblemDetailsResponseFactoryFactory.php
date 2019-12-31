81bdfab9cf4ac4358eaf953bfc6c1c1e9e8cb926
Matthew Weier O'Phinney <matthew@weierophinney.net>
2019-12-31 09:45:35 -0600

    Rewriting as Laminas Project package

--- src/ProblemDetailsResponseFactoryFactory.php
+++ src/ProblemDetailsResponseFactoryFactory.php
@@ -1,11 +1,12 @@
 <?php
+
 /**
- * @see       https://github.com/zendframework/zend-problem-details for the canonical source repository
- * @copyright Copyright (c) 2017 Zend Technologies USA Inc. (http://www.zend.com)
- * @license   https://github.com/zendframework/zend-problem-details/blob/master/LICENSE.md New BSD License
+ * @see       https://github.com/mezzio/mezzio-problem-details for the canonical source repository
+ * @copyright https://github.com/mezzio/mezzio-problem-details/blob/master/COPYRIGHT.md
+ * @license   https://github.com/mezzio/mezzio-problem-details/blob/master/LICENSE.md New BSD License
  */
 
-namespace Zend\ProblemDetails;
+namespace Mezzio\ProblemDetails;
 
 use Psr\Container\ContainerInterface;
 use Psr\Http\Message\ResponseInterface;
@@ -24,8 +25,8 @@ class ProblemDetailsResponseFactoryFactory
             ? $container->get(ResponseInterface::class)
             : null;
 
-        $streamFactory = $container->has('Zend\ProblemDetails\StreamFactory')
-            ? $container->get('Zend\ProblemDetails\StreamFactory')
+        $streamFactory = $container->has('Mezzio\ProblemDetails\StreamFactory')
+            ? $container->get('Mezzio\ProblemDetails\StreamFactory')
             : null;
 
         return new ProblemDetailsResponseFactory(
