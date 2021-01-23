47966f3756b22ccff751fe53044028a3890bbc9f
Alejandro Celaya <alejandro@alejandrocelaya.com>
2021-01-23 16:44:48 +0100

    Explicitly imported legacy classes with the Legacy prefix
    Signed-off-by: Alejandro Celaya <alejandro@alejandrocelaya.com>
    Signed-off-by: Alejandro Celaya <alejandrocelaya@gmail.com>

--- src/ConfigProvider.php
+++ src/ConfigProvider.php
@@ -10,12 +10,14 @@ declare(strict_types=1);
 
 namespace Mezzio\ProblemDetails;
 
+use Zend\ProblemDetails\ProblemDetailsMiddleware as LegacyProblemDetailsMiddleware;
+use Zend\ProblemDetails\ProblemDetailsNotFoundHandler as LegacyProblemDetailsNotFoundHandler;
+use Zend\ProblemDetails\ProblemDetailsResponseFactory as LegacyProblemDetailsResponseFactory;
+
 /**
  * Configuration provider for the package.
  *
  * @see https://docs.laminas.dev/laminas-component-installer/
- *
- * phpcs:disable WebimpressCodingStandard.PHP.DisallowFqn.FileName
  */
 class ConfigProvider
 {
@@ -37,9 +39,9 @@ class ConfigProvider
         return [
             // Legacy Zend Framework aliases
             'aliases'   => [
-                \Zend\ProblemDetails\ProblemDetailsMiddleware::class      => ProblemDetailsMiddleware::class,
-                \Zend\ProblemDetails\ProblemDetailsNotFoundHandler::class => ProblemDetailsNotFoundHandler::class,
-                \Zend\ProblemDetails\ProblemDetailsResponseFactory::class => ProblemDetailsResponseFactory::class,
+                LegacyProblemDetailsMiddleware::class      => ProblemDetailsMiddleware::class,
+                LegacyProblemDetailsNotFoundHandler::class => ProblemDetailsNotFoundHandler::class,
+                LegacyProblemDetailsResponseFactory::class => ProblemDetailsResponseFactory::class,
             ],
             'factories' => [
                 ProblemDetailsMiddleware::class      => ProblemDetailsMiddlewareFactory::class,
