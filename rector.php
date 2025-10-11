1e95e20a81c1fe3bf1aeeae5287393626e4546f5
mmalac <mmalac@sygic.com>
2025-10-11 21:19:05 +0200

    update: switch Rector configuration to PHP 8.2
    Signed-off-by: mmalac <mmalac@sygic.com>

--- rector.php
+++ rector.php
@@ -5,7 +5,7 @@ declare(strict_types=1);
 use Rector\Config\RectorConfig;
 
 return RectorConfig::configure()
-    ->withPhpSets(php81: true)
+    ->withPhpSets(php82: true)
     ->withPaths([
         __DIR__ . '/src',
         __DIR__ . '/test',
